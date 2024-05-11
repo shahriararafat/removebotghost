02aff30b1fe3911e16ffcf6a2dc755473720e280
Shahriar Arafat <arafatshahriar98@gmail.com>
2024-05-12 02:25:12 +0600

    index.js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/**
- ██████╗░████████╗██╗░░██╗           
- ██╔══██╗╚══██╔══╝╚██╗██╔╝          
- ██████╔╝░░░██║░░░░╚███╔╝░          
- ██╔══██╗░░░██║░░░░██╔██╗░          
- ██║░░██║░░░██║░░░██╔╝╚██╗          
- ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-  GIT : https://github.com/RTX-GAMINGG/Bot-ghost-status-remover-by-RTX
-  DISCORD SERVER : https://discord.gg/FUEHs7RCqz
-  YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
- * **********************************************
- *   Code by RTX GAMING
- * **********************************************
- */
-
-
-
 const { Client, GatewayIntentBits, ActivityType, TextChannel } = require('discord.js');
 require('dotenv').config();
 const express = require('express');
@@ -36,7 +19,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Shahriar Arafat"];
+const statusMessages = ["⚒️Listening to Shahriar Arafat"];
 
 
 let currentIndex = 0;
