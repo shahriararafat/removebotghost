3ca93b4f4a8ec5f48400be96c70c85c8c175526a
Shahriar Arafat <arafatshahriar98@gmail.com>
2024-05-12 01:23:16 +0600

    index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Listening to Shahriar Arafat: http://localhost:${port}`);
+  console.log(`🔗 Powered By Shahriar Arafat`);
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Shahriar Arafat"];
 
 
 let currentIndex = 0;
